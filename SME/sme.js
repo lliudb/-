03aa6a83a634a43c44f35c2775f94b1bc7edcf20
doc.D <ggplace@qq.com>
2016-08-02 15:35:48 +0800

    no message

--- SME/sme.js
+++ SME/sme.js
@@ -1,3 +1,56 @@
 ;(function($WIN,$){
-	
+
+	var opts = {
+		menu:[
+			{
+				ico:"ico-x",
+				text:"地貌",
+				sub:[	
+						{
+							ico:"ico-x1",
+							text:"山",
+							type:1,
+						},{
+							ico:"ico-x2",
+							text:"川",
+							type:2,
+						},{
+							ico:"ico-x3",
+							text:"城",
+							type:3,
+						}
+					]
+			},
+
+		],
+		option:[
+			{
+				ico:"ico-x",
+				text:"保存",
+				click:function(items,curr){return null;},
+			},{
+				ico:"ico-x",
+				text:"撤销",
+				click:function(items,curr){return null;},
+			},{
+				ico:"ico-x",
+				text:"重绘",
+				click:function(items,curr){return null;},
+			},{
+				ico:"ico-x",
+				text:"删除",
+				click:function(items,curr){return null;},
+			}
+		]
+	}
+
+	var SME = function(config,opt){
+		var config = {
+			version:"0.1.0",
+
+		};
+
+		return config{version:"0.1.0"};
+	}
+
 })(window,Jquery)
