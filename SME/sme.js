dfa960c7f309f536c7169384e2ef603043e635ea
doc.D <ggplace@qq.com>
2016-08-03 10:52:52 +0800

    no message

--- SME/sme.js
+++ SME/sme.js
@@ -1,56 +1,45 @@
 ;(function($WIN,$){
 
-	var opts = {
-		menu:[
-			{
-				ico:"ico-x",
-				text:"地貌",
-				sub:[	
-						{
-							ico:"ico-x1",
-							text:"山",
-							type:1,
-						},{
-							ico:"ico-x2",
-							text:"川",
-							type:2,
-						},{
-							ico:"ico-x3",
-							text:"城",
-							type:3,
-						}
-					]
-			},
+	// var opts = {
+	// 	menu:[
+	// 		{
+	// 			ico:"ico-x",
+	// 			text:"地貌",
+	// 			sub:[{ico:"ico-x1",text:"山",type:1,},
+	// 					{ico:"ico-x2",text:"川",type:2,},
+	// 					{ico:"ico-x3",text:"城",type:3,}]
+	// 		},
+	// 	],
+	// 	option:[
+	// 		{ico:"ico-x",text:"保存",click:function(items,curr){return null;},},
+	// 		{ico:"ico-x",text:"撤销",click:function(items,curr){return null;},},
+	// 		{ico:"ico-x",text:"重绘",click:function(items,curr){return null;},},
+	// 		{ico:"ico-x",text:"删除",click:function(items,curr){return null;},}
+	// 	]
+	// }
 
-		],
-		option:[
-			{
-				ico:"ico-x",
-				text:"保存",
-				click:function(items,curr){return null;},
-			},{
-				ico:"ico-x",
-				text:"撤销",
-				click:function(items,curr){return null;},
-			},{
-				ico:"ico-x",
-				text:"重绘",
-				click:function(items,curr){return null;},
-			},{
-				ico:"ico-x",
-				text:"删除",
-				click:function(items,curr){return null;},
-			}
-		]
-	}
-
-	var SME = function(config,opt){
-		var config = {
+	var SME = function(content,config){
+		var that = this;
+		that.__cont = document.getElementById(content);
+		that.config = {
 			version:"0.1.0",
+			content_w:that.__cont.clientWidth,
+			content_h:that.__cont.clientHeight,
 
 		};
-
-		return config{version:"0.1.0"};
+ 			
+ 		that.init = function(){
+ 			that.__canvas = document.getElementById("SmeCanvas");
+ 			console.log(that)
+ 			that.__canvas.clientWidth = that.config.content_w;
+			that.__canvas.clientHeight = that.config.content_h;
+ 			return that;
+ 		}
+ 
+		return that;
 	}
 
-})(window,Jquery)
+
+	$WIN.SME = SME;
+
+})(window,$)
